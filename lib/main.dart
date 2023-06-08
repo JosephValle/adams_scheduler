9eec6ec42aaa6fe6cf450bf55f78840d23edf4d6
JosephValle <josephv2599@gmail.com>
2023-06-08 15:10:02 -0400

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
 import "package:scheduler/bloc/auth/auth_bloc.dart";
-import 'package:scheduler/ui/home/home_screen.dart';
+import "package:scheduler/ui/home/home_screen.dart";
 
 void main() {
   runApp(const MyApp());
@@ -13,18 +13,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-        providers: [
-          BlocProvider<AuthBloc>(
-            create: (context) => AuthBloc(),
-          ),
-        ],
-        child: MaterialApp(
-          title: "Flutter Demo",
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-            useMaterial3: true,
-          ),
-          home: const HomeScreen(),
-        ),);
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (context) => AuthBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        title: "Flutter Demo",
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
   }
 }
